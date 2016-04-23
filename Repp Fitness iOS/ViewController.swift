357fea5877eb23d42b71db0e17e3e53d5bf9b617
James Ormond <jormond@andrew.cmu.edu>
2016-04-23 13:27:11 -0400

    added working web view

--- Repp Fitness iOS/ViewController.swift
+++ Repp Fitness iOS/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainView: UIWebView!
+    var webView: WKWebView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var url = NSURL(string:"http://www.facebook.com") // make a URL
+        var req = NSURLRequest(URL:url!) // make a request w/ that URL
+        self.webView = WKWebView() // instantiate WKWebView
+        self.webView!.loadRequest(req) // unwrap the webView and load the request.
+        self.view = self.webView! // make it the main view
     }
 
     override func didReceiveMemoryWarning() {
